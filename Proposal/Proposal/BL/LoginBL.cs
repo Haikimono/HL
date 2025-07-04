67ae517d333a2fd290221c30835e2f657e09b6b3
teki <tekikeiki@gmail.com>
2025-07-04 13:42:45 +0900

    add commit

---
+++ Proposal/Proposal/BL/LoginBL.cs
@@ -0,0 +1,36 @@
+﻿using Proposal.DAC;
+using Proposal.Models;
+
+namespace Proposal.BL
+{
+    public class LoginBL
+    {
+        private readonly LoginDAC _LoginDAC;
+
+        public LoginBL()
+        {
+        }
+
+        public LoginBL(string connectionString)
+        {
+            _LoginDAC = new LoginDAC(connectionString);
+        }
+
+        /// <summary>
+        /// 验证用户账号密码
+        /// </summary>
+        /// <returns>登录成功返回用户对象，否则返回 null</returns>
+        public bool ValidateUser(string userId, string password)
+        {
+            var user = _LoginDAC.GetUserById(userId);
+            if (user != null && user.Password == password) 
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
