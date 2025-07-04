6dbdec68ef4e99092095df1ad1c380ff3ba184e2
teki <tekikeiki@gmail.com>
2025-07-04 17:10:56 +0900

    20250704/teki

--- Proposal/Proposal/Controllers/MenuController.cs
+++ Proposal/Proposal/Controllers/MenuController.cs
@@ -12,5 +12,21 @@ namespace Proposal.Controllers
             HttpContext.Session.Clear();
             return View("~/Views/Login/Login.cshtml");
         }
+
+
+        [Route("proposal/list")]
+        public IActionResult List()
+        {
+            // 返回列表视图
+            return View("~/Views/Menu/List.cshtml");
+        }
+
+
+        [Route("proposal/create")]
+        public IActionResult Create ()
+        {
+            // 返回列表视图
+            return View("~/Views/Menu/create.cshtml");
+        }
     }
 }
